2e14d4f71414bbcea40e28fa6d6a8fb207cbd70b
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2021-09-21 14:24:28 +0530

    feat - quiz

---
+++ README.md
@@ -0,0 +1,17 @@
+# markTwo NEOGCAMP Project
+
+## Overview
+
+### What is it ?
+
+- A simple fun quiz on Nutrition.
+- Checks the user's answer.
+- Updates the user's score.
+
+### Built with
+
+- A CLI project in Node.js.
+- readline-sync to read user input.
+- Functional programming.
+
+[markTwo Quiz Replit](https://replit.com/@foolhardy21/mktwo?v=1)
---
+++ quizdata.js
@@ -0,0 +1,22 @@
+module.exports = [
+    {
+      ques: 'Primary nutrient for muscle building?',
+      ans: 'Protein'
+    },
+    {
+      ques: 'Diet plan where one only eats once or twice in 24 hrs?',
+      ans: 'Intermittent Fasting'
+    },
+    {
+      ques: 'Acronym for ( (weight in kg)/(height in m)^2 )?',
+      ans: 'BMI'
+    },
+    {
+      ques: 'Carbs are a form of',
+      ans: 'Sugar'
+    },
+    {
+      ques: 'Citric fruits have Vitamin __?',
+      ans: 'C'
+    },
+  ]
